fd6aa95de7077378df655e342c493cd7d9e23506
Joe Palala <jose.palala@live.com>
2018-12-05 22:42:32 +0800

    create meetups table w/ lat long

--- restapi/database/migrations/1544018127250_meetups_schema.js
+++ restapi/database/migrations/1544018127250_meetups_schema.js
@@ -7,6 +7,15 @@ class MeetupsSchema extends Schema {
   up () {
     this.create('meetups', (table) => {
       table.increments()
+      table.string('title').unique();
+      table.string('summary_image_url')->nullable();
+      table.text('detail');
+      table.datetime('start_date').defaultTo(knex.fn.now());
+      table->dateTime('end_date').defaultTo(knex.fn.now());
+      table.string('address');
+      table.decimal('latitude');
+      table.decimal('longitude');
+      table->string('city');
       table.timestamps()
     })
   }
