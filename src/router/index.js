b6d75247431c1cf5069bb2dba92dd47ddebda328
Joe Palala <jose.palala@live.com>
2019-02-05 10:38:11 +0800

    added components to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import UserLogout from '@/components/UserLogout'
+import Dashboard from '@/components/hboard'
+import Dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
 
@@ -8,8 +11,24 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/user/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/user/logout',
+      name: 'Logout',
+      component: UserLogout
+    },
+    //handled by Laravel app, if path is login, http-redirect to be done to laravel app login:
+    {
+      path: '/user/login',
+      name: 'Login',
+      component: UserLogout
     }
   ]
 })
