52c533c36dd64fea19bc8e18d291e513f7f0fee5
Joe Palala <622659+jpalala@users.noreply.github.com>
2019-05-21 09:36:19 +0800

    vue components for reference that we already had

---
+++ Vue/Profile.vue
@@ -0,0 +1,53 @@
+<template>
+   <div>You are {{username}}</div>
+   <div>
+   Do you want to change password?
+   </div>
+    <h2>Using bootstrap vue</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          This will be the Profile / Settings for the user page
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue());
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      username: 'whoami'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Vue/Stats.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stats">
+    <h1>Look at all the meetups you can signup for!</h1>
+    <p>There are currently {{ countMeetups }} links</p>
+  </div>
+</template>
+
+<script>
+//import { mapGetters } from 'vuex'
+import meetupCount as meetupCountService from 'services/meetupcount'
+
+export default {
+  name: 'DashboardStats',
+  computed: {
+    meetups: {
+       getCount: function() {
+          return  meetupCountService.get();
+       }
+
+  },
+  methods: {
+  }
+
+}
+</script>
